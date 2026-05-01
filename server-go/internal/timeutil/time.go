977bbc293d7da5612640ede3e9bcac9cee5afd30
qiuwei <qiuwei92@hotmail.com>
2026-05-01 20:02:38 +0800

    fix(go): 修复容器时区兜底

--- server-go/internal/timeutil/time.go
+++ server-go/internal/timeutil/time.go
@@ -7,7 +7,10 @@ const format = "2006-01-02 15:04:05"
 func LocalTimestamp(locationName string) string {
 	loc, err := time.LoadLocation(locationName)
 	if err != nil {
-		loc, _ = time.LoadLocation("Asia/Shanghai")
+		loc, err = time.LoadLocation("Asia/Shanghai")
+	}
+	if err != nil {
+		loc = time.FixedZone("Asia/Shanghai", 8*60*60)
 	}
 	return time.Now().In(loc).Format(format)
 }
